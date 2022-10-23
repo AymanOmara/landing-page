1460f9d98b2e0ac025edb6a312e7bac44050bf45
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-10-23 17:42:10 +0200

    create nav bar useing bootstrap and add imeges for the landing page

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LandingPage from "./landing_page/landing-page.jsx";
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
---
+++ src/landing_page/landing-page.jsx
@@ -0,0 +1,85 @@
+import { Component } from "react";
+import "./landing-page.css";
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar sticky-top navbar-expand-lg nav-bar-background navbar-light bg-light">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="#navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand">Ayman </a>
+          <ul class="navbar-nav ms-auto">
+          <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item">Action</a>
+          <a className="dropdown-item">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                contact
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                Download
+              </a>
+            </li>
+            
+            
+      
+            
+          </ul>
+        </div>
+      </nav>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      <h1>Ayman</h1>
+      </div>
+    );
+  }
+}
